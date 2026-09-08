c3cbbc8f06fbbe422125e64279a90d9d64e46b29
S P Sooryananda <sooryananda18@gmail.com>
2026-09-08 13:17:17 +0530

    Initial student information app

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("showDetailsBtn").addEventListener("click", function () {
+  document.getElementById("detailsOutput").textContent =
+    "Student details are fully loaded and verified.";
+});
